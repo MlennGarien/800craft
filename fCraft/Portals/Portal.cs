0dfd6a9f192ad4a9bd1d37d9785b5b8b7d10fb51
Glenn Marien <glennd600@hotmail.com>
2011-11-13 21:16:13 +0100

    Portals, brah

---
+++ fCraft/Portals/Portal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft.Portals
+{
+    public class Portal
+    {
+        public World World { get; set; }
+        public Vector3I[] AffectedBlocks { get; set; }
+        public PortalRange Range { get; set; }
+
+        public Portal(World world, Vector3I[] affectedBlocks)
+        {
+            this.World = world;
+            this.AffectedBlocks = affectedBlocks;
+            this.Range = Portal.CalculateRange(this);
+        }
+
+        public static PortalRange CalculateRange(Portal portal)
+        {
+            PortalRange range = new PortalRange(0, 0, 0, 0, 0, 0);
+
+            foreach (Vector3I block in portal.AffectedBlocks)
+            {
+                if (block.X < portal.Range.Xmin)
+                {
+                    range.Xmin = block.X;
+                }
+
+                if (block.X > portal.Range.Xmax)
+                {
+                    range.Xmax = block.X;
+                }
+
+                if (block.Y < portal.Range.Ymin)
+                {
+                    range.Ymin = block.Y;
+                }
+
+                if (block.Y > portal.Range.Ymax)
+                {
+                    range.Ymax = block.Y;
+                }
+
+                if (block.Z < portal.Range.Zmin)
+                {
+                    range.Zmin = block.Z;
+                }
+
+                if (block.Z > portal.Range.Zmax)
+                {
+                    range.Zmax = block.Z;
+                }
+            }
+
+            return range;
+        }
+
+        public bool IsInRange(Player player)
+        {
+            if (player.Position.X <= Range.Xmax && player.Position.X >= Range.Xmin)
+            {
+                if (player.Position.Y <= Range.Ymax && player.Position.Y >= Range.Ymin)
+                {
+                    if (player.Position.Z <= Range.Zmax && player.Position.Z >= Range.Zmin)
+                    {
+                        return true;
+                    }
+                }
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ fCraft/Portals/PortalHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft.Portals
+{
+    class PortalHandler
+    {
+        private static PortalHandler instance;
+
+        private PortalHandler()
+        {
+            // Empty, singleton
+        }
+
+        public static PortalHandler GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PortalHandler();
+                Player.Moved += new EventHandler<Events.PlayerMovedEventArgs>(Player_Moved);
+            }
+
+            return instance;
+        }
+
+        static void Player_Moved(object sender, Events.PlayerMovedEventArgs e)
+        {
+            if (e.Player.Can(Permission.UsePortal))
+            {
+                if (PortalHandler.GetInstance().GetPortal(e.Player) != null)
+                {
+                    Portal portal = PortalHandler.GetInstance().GetPortal(e.Player);
+                    e.Player.Message("Portal found");
+                }
+            }
+        }
+
+        public Portal GetPortal(Player player)
+        {
+            Portal portal = null;
+
+            try
+            {
+                if (player.World.Portals != null && player.World.Portals.Count > 0)
+                {
+                    lock (player.World.Portals.SyncRoot)
+                    {
+                        foreach (Portal possiblePortal in player.World.Portals)
+                        {
+                            if (possiblePortal.IsInRange(player))
+                            {
+                                return possiblePortal;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(LogType.Error, "PortalHandler.GetPortal: " + ex);
+            }
+
+            return portal;
+        }
+
+        public static void CreatePortal(World world, Vector3I[] affectedBlocks)
+        {
+            Portal portal = new Portal(world, affectedBlocks);
+
+            lock (world.Portals.SyncRoot)
+            {
+                world.Portals.Add(portal);
+            }
+        }
+    }
+}
