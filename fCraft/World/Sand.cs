94dea4fb04ac9f5e2800b1c0ab7daeb2edfea563
Jonty800 <Jonty800@gmail.com>
2011-11-18 14:47:30 +0000

    Minor commenting on Sand.cs

--- fCraft/World/Sand.cs
+++ fCraft/World/Sand.cs
@@ -9,14 +9,15 @@ namespace fCraft
     {
         static World world;
         static HashSet<Block> sandFall = new HashSet<Block>() { Block.Air, Block.Water, Block.Lava, Block.StillWater, Block.StillLava };
+        //^things sand can fall into
         public static void Init(World world_)
         {
             world = world_;
         }
 
-        public static void SandTrigger(Player player, int x, int y, int z, Block type)
+        public static void SandTrigger(Player player, int x, int y, int z, Block type) //trigger
         {
-            if (type == Block.Sand )
+            if (type == Block.Sand || type == Block.Gravel) //supported blocks
             {
                 World world = player.World;
                 int dropHeight = Drop(x, y, z);
@@ -35,7 +36,7 @@ namespace fCraft
             int y = dy + sy;
             int z = dh + sh;
             Block type = (Block)world.Map.GetBlock(x, y, z);
-            if (type == Block.Sand )
+            if (type == Block.Sand || type == Block.Gravel) //if sand or gravel, go to Drop()
             {
                 int dropHeight = Drop(x, y, z);
                 if (dropHeight != z)
@@ -73,14 +74,14 @@ namespace fCraft
                 Propagate(x, y, z, 0, 0, -1);
             Propagate(x, y, z, 0, 0, 1);
         }
-        static void Send(int x, int y, int z, int fh, Block type)
+        static void Send(int x, int y, int z, int fh, Block type) //for use with Propagate
         {
             world.Map.QueueUpdate(new BlockUpdate(Player.Console, (short)x, (short)y, (short)z, 0));
             world.Map.QueueUpdate(new BlockUpdate(Player.Console, (short)x, (short)y, (short)fh, (Block)(byte)type));
             world.Map.SetBlock(x, y, z, Block.Air);
             world.Map.SetBlock(x, y, fh, type);
         }
-        static void Send(int x, int y, int z, int fh, Block type, Player player)
+        static void Send(int x, int y, int z, int fh, Block type, Player player) //for use with sandtrigger
         {
             player.SendNow(PacketWriter.MakeSetBlock(x, y, z, 0));
             player.SendNow(PacketWriter.MakeSetBlock(x, y, fh, (Block)(byte)type));
@@ -94,7 +95,7 @@ namespace fCraft
         {
             if (z == 0)
                 return 0;
-            while (sandFall.Contains((Block)world.Map.GetBlock(x, y, z - 1)) && --z > 0) ; //WIN
+            while (sandFall.Contains((Block)world.Map.GetBlock(x, y, z - 1)) && --z > 0) ; //crank dat soulja boy
             return z;
         }
     }
