1b1e228306eff107dcb5ca596022e3f4dfc36cea
Jonty800 <Jonty800@gmail.com>
2011-11-20 18:33:47 +0000

    cosmetic changes to the updater

--- ServerGUI/UpdateWindow.Designer.cs
+++ ServerGUI/UpdateWindow.Designer.cs
@@ -37,73 +37,73 @@
             // lHeader
             // 
             this.lHeader.AutoSize = true;
-            this.lHeader.Font = new System.Drawing.Font( "Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)) );
-            this.lHeader.Location = new System.Drawing.Point( 12, 9 );
+            this.lHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lHeader.Location = new System.Drawing.Point(12, 9);
             this.lHeader.Name = "lHeader";
-            this.lHeader.Size = new System.Drawing.Size( 187, 13 );
+            this.lHeader.Size = new System.Drawing.Size(204, 13);
             this.lHeader.TabIndex = 5;
-            this.lHeader.Text = "An update to fCraft is available!";
+            this.lHeader.Text = "An update to 800Craft is available!";
             // 
             // bCancel
             // 
             this.bCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.bCancel.Location = new System.Drawing.Point( 472, 279 );
+            this.bCancel.Location = new System.Drawing.Point(480, 234);
             this.bCancel.Name = "bCancel";
-            this.bCancel.Size = new System.Drawing.Size( 100, 23 );
+            this.bCancel.Size = new System.Drawing.Size(100, 23);
             this.bCancel.TabIndex = 4;
             this.bCancel.Text = "Cancel";
             this.bCancel.UseVisualStyleBackColor = true;
-            this.bCancel.Click += new System.EventHandler( this.bCancel_Click );
+            this.bCancel.Click += new System.EventHandler(this.bCancel_Click);
             // 
             // bUpdateNow
             // 
             this.bUpdateNow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bUpdateNow.Enabled = false;
-            this.bUpdateNow.Location = new System.Drawing.Point( 260, 279 );
+            this.bUpdateNow.Location = new System.Drawing.Point(268, 234);
             this.bUpdateNow.Name = "bUpdateNow";
-            this.bUpdateNow.Size = new System.Drawing.Size( 100, 23 );
+            this.bUpdateNow.Size = new System.Drawing.Size(100, 23);
             this.bUpdateNow.TabIndex = 2;
             this.bUpdateNow.Text = "Restart Now";
             this.bUpdateNow.UseVisualStyleBackColor = true;
-            this.bUpdateNow.Click += new System.EventHandler( this.bUpdateNow_Click );
+            this.bUpdateNow.Click += new System.EventHandler(this.bUpdateNow_Click);
             // 
             // bUpdateLater
             // 
             this.bUpdateLater.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.bUpdateLater.Enabled = false;
-            this.bUpdateLater.Location = new System.Drawing.Point( 366, 279 );
+            this.bUpdateLater.Location = new System.Drawing.Point(374, 234);
             this.bUpdateLater.Name = "bUpdateLater";
-            this.bUpdateLater.Size = new System.Drawing.Size( 100, 23 );
+            this.bUpdateLater.Size = new System.Drawing.Size(100, 23);
             this.bUpdateLater.TabIndex = 3;
             this.bUpdateLater.Text = "Update Later";
             this.bUpdateLater.UseVisualStyleBackColor = true;
-            this.bUpdateLater.Click += new System.EventHandler( this.bUpdateLater_Click );
+            this.bUpdateLater.Click += new System.EventHandler(this.bUpdateLater_Click);
             // 
             // progress
             // 
             this.progress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.progress.Location = new System.Drawing.Point( 436, 12 );
+            this.progress.Location = new System.Drawing.Point(444, 12);
             this.progress.Name = "progress";
-            this.progress.Size = new System.Drawing.Size( 136, 23 );
+            this.progress.Size = new System.Drawing.Size(136, 23);
             this.progress.TabIndex = 7;
             // 
             // lProgress
             // 
             this.lProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lProgress.AutoSize = true;
-            this.lProgress.Location = new System.Drawing.Point( 433, 38 );
+            this.lProgress.Location = new System.Drawing.Point(441, 38);
             this.lProgress.Name = "lProgress";
-            this.lProgress.Size = new System.Drawing.Size( 100, 13 );
+            this.lProgress.Size = new System.Drawing.Size(100, 13);
             this.lProgress.TabIndex = 8;
             this.lProgress.Text = "Downloading ({0}%)";
             // 
             // lVersion
             // 
             this.lVersion.AutoSize = true;
-            this.lVersion.Location = new System.Drawing.Point( 12, 25 );
+            this.lVersion.Location = new System.Drawing.Point(12, 25);
             this.lVersion.Name = "lVersion";
-            this.lVersion.Size = new System.Drawing.Size( 266, 26 );
+            this.lVersion.Size = new System.Drawing.Size(266, 26);
             this.lVersion.TabIndex = 6;
             this.lVersion.Text = "Currently installed version: {0}\r\nNewest available version: {1} (released {2:0} d" +
                 "ays ago)";
@@ -113,52 +113,53 @@
             this.tChangeLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
                         | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
-            this.tChangeLog.Location = new System.Drawing.Point( 12, 70 );
+            this.tChangeLog.Location = new System.Drawing.Point(12, 70);
             this.tChangeLog.Multiline = true;
             this.tChangeLog.Name = "tChangeLog";
             this.tChangeLog.ReadOnly = true;
             this.tChangeLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.tChangeLog.Size = new System.Drawing.Size( 560, 203 );
+            this.tChangeLog.Size = new System.Drawing.Size(568, 158);
             this.tChangeLog.TabIndex = 0;
             // 
             // xShowDetails
             // 
             this.xShowDetails.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.xShowDetails.AutoSize = true;
-            this.xShowDetails.Location = new System.Drawing.Point( 12, 283 );
+            this.xShowDetails.Location = new System.Drawing.Point(12, 238);
             this.xShowDetails.Name = "xShowDetails";
-            this.xShowDetails.Size = new System.Drawing.Size( 86, 17 );
+            this.xShowDetails.Size = new System.Drawing.Size(86, 17);
             this.xShowDetails.TabIndex = 1;
             this.xShowDetails.Text = "Show details";
             this.xShowDetails.UseVisualStyleBackColor = true;
-            this.xShowDetails.CheckedChanged += new System.EventHandler( this.xShowDetails_CheckedChanged );
+            this.xShowDetails.CheckedChanged += new System.EventHandler(this.xShowDetails_CheckedChanged);
             // 
             // UpdateWindow
             // 
             this.AcceptButton = this.bUpdateLater;
-            this.AutoScaleDimensions = new System.Drawing.SizeF( 6F, 13F );
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.CancelButton = this.bCancel;
-            this.ClientSize = new System.Drawing.Size( 584, 314 );
-            this.Controls.Add( this.xShowDetails );
-            this.Controls.Add( this.tChangeLog );
-            this.Controls.Add( this.lVersion );
-            this.Controls.Add( this.bUpdateLater );
-            this.Controls.Add( this.bUpdateNow );
-            this.Controls.Add( this.bCancel );
-            this.Controls.Add( this.lProgress );
-            this.Controls.Add( this.lHeader );
-            this.Controls.Add( this.progress );
+            this.ClientSize = new System.Drawing.Size(592, 269);
+            this.Controls.Add(this.xShowDetails);
+            this.Controls.Add(this.tChangeLog);
+            this.Controls.Add(this.lVersion);
+            this.Controls.Add(this.bUpdateLater);
+            this.Controls.Add(this.bUpdateNow);
+            this.Controls.Add(this.bCancel);
+            this.Controls.Add(this.lProgress);
+            this.Controls.Add(this.lHeader);
+            this.Controls.Add(this.progress);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size( 500, 130 );
+            this.MinimumSize = new System.Drawing.Size(500, 130);
             this.Name = "UpdateWindow";
             this.ShowIcon = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "fCraft Updater";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler( this.UpdateWindow_FormClosing );
-            this.ResumeLayout( false );
+            this.Text = "800Craft Updater";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UpdateWindow_FormClosing);
+            this.ResumeLayout(false);
             this.PerformLayout();
 
         }
