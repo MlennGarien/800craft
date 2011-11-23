1ddf5544ff4e111dd157fafea6f7a9fa49fad77b
Glenn Marien <glennd600@hotmail.com>
2011-11-23 19:17:45 +0100

    Added GrassUpdate.cs

---
+++ fCraft/Physics/GrassUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft.Physics
+{
+    class GrassUpdate
+    {
+        public World World { get; set; }
+        public Vector3I Block { get; set; }
+        public DateTime Scheduled { get; set; }
+
+        public GrassUpdate(World World, Vector3I Block, DateTime Scheduled)
+        {
+            this.World = World;
+            this.Block = Block;
+            this.Scheduled = Scheduled;
+        }
+    }
+}
