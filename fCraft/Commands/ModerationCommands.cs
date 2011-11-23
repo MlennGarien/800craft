84ae99de1762087d3f1083906a596b75121de3fd
Jonty800 <Jonty800@gmail.com>
2011-11-23 17:29:12 +0000

    warn update

--- fCraft/Commands/ModerationCommands.cs
+++ fCraft/Commands/ModerationCommands.cs
@@ -566,7 +566,7 @@ namespace fCraft {
                 return;
             }
 
-            if (player.Can(Permission.Ban, target.Info.Rank))
+            if (player.Can(Permission.Warn, target.Info.Rank))
             {
                 target.Info.IsHidden = false;
                 if (target.Info.Warn(player.Name))
@@ -597,7 +597,7 @@ namespace fCraft {
             else
             {
                 player.Message("You can only warn players ranked {0}&S or lower",
-                                player.Info.Rank.GetLimit(Permission.Kick).ClassyName);
+                                player.Info.Rank.GetLimit(Permission.Ban).ClassyName);
                 player.Message("{0}&S is ranked {1}", target.ClassyName, target.Info.Rank.ClassyName);
             }
         }
@@ -633,7 +633,7 @@ namespace fCraft {
                 return;
             }
 
-            if (player.Can(Permission.Ban, target.Info.Rank))
+            if (player.Can(Permission.Warn, target.Info.Rank))
             {
                 if (target.Info.UnWarn())
                 {
@@ -647,7 +647,7 @@ namespace fCraft {
             else
             {
                 player.Message("You can only unwarn players ranked {0}&S or lower",
-                                player.Info.Rank.GetLimit(Permission.Kick).ClassyName);
+                                player.Info.Rank.GetLimit(Permission.Warn).ClassyName);
                 player.Message("{0}&S is ranked {1}", target.ClassyName, target.Info.Rank.ClassyName);
             }
         }
