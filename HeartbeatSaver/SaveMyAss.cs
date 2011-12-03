68e4d0c3a82b6c51cd7be3dc8e165a91e6ccf17f
Jonty800 <Jonty800@gmail.com>
2011-12-03 04:31:25 +0000

    This works better

--- HeartbeatSaver/SaveMyAss.cs
+++ HeartbeatSaver/SaveMyAss.cs
@@ -10,47 +10,52 @@ namespace HeartbeatSender
 {
     class HeartbeatSender
     {
+        public static bool On = true;
+
+
         static void Main()
         {
-            bool on = true;
             string port = Server.Port.ToString();
             string Salt = Heartbeat.Salt;
             string Name = ConfigKey.ServerName.GetString();
-            
-            do
-            {
-                // this is what we are sending
-                StringBuilder sb = new StringBuilder();
-            sb.AppendFormat( "public=True&max=20&users=20&port={0}&version=7&salt={1}&name=HeartBeatSaver", port, Salt, Name);
+
+            // this is what we are sending
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("public=True&max=20&users=120&port=25565&version=7&salt={1}&name=Au70Galaxy", port, Salt, Name);
             string post_data = sb.ToString();
-            Console.WriteLine("Sending " + sb.ToString()+"\n");
-            
-
-                // this is where we will send it
-                string uri = "http://www.minecraft.net/heartbeat.jsp";
-
-                // create a request
-                HttpWebRequest request = (HttpWebRequest)
-                WebRequest.Create(uri); request.KeepAlive = true;
-                request.ProtocolVersion = HttpVersion.Version10;
-                request.Method = "POST";
-
-                // turn our request string into a byte stream
-                byte[] postBytes = Encoding.ASCII.GetBytes(post_data);
+            Console.WriteLine("Sending " + sb.ToString() + "\n");
 
 
-                request.ContentType = "application/x-www-form-urlencoded";
-                request.ContentLength = postBytes.Length;
-                Stream requestStream = request.GetRequestStream();
+            // this is where we will send it
+            string uri = "http://www.minecraft.net/heartbeat.jsp";
 
-                // send it
-                requestStream.Write(postBytes, 0, postBytes.Length);
-                requestStream.Close();
+            // create a request
+            HttpWebRequest request = (HttpWebRequest)
+            WebRequest.Create(uri); request.KeepAlive = false;
+            request.ProtocolVersion = HttpVersion.Version10;
+            request.Method = "POST";
 
-                Console.WriteLine("Sent\n");
-            }
-            while (on);
+            // turn our request string into a byte stream
+            byte[] postBytes = Encoding.ASCII.GetBytes(post_data);
+
+
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = postBytes.Length;
+            Stream requestStream = request.GetRequestStream();
+
+            // send it
+
+            requestStream.Write(postBytes, 0, postBytes.Length);
+            requestStream.Close();
+
+            Console.WriteLine("Sent\n");
+            On = false;
+
+            Thread.Sleep(15000);
+            Main();
         }
     }
+   
+
 }
 
