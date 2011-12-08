b09116c0f9321dfe8a8ffbd94a7552abfe271efb
Glenn Marien <glennd600@hotmail.com>
2011-12-08 23:29:59 +0100

    Here jonty, you hater

---
+++ fCraft/Drawing/DrawOps/TreeDrawOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft.Drawing
+{
+    class TreeDrawOperation : DrawOperation
+    {
+        public override int ExpectedMarks
+        {
+            get { return 1; }
+        }
+
+        public TreeDrawOperation(Player player)
+            : base(player)
+        {
+            // Empty
+        }
+
+        public override string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override int DrawBatch(int maxBlocksToDraw)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
