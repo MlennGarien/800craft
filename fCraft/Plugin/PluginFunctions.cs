1ad178744459cfa18eaf21e68df98e7d1193e07c
Glenn Marien <glennd600@hotmail.com>
2011-12-08 23:22:25 +0100

    Added LUA binaries

---
+++ fCraft/Plugin/PluginFunctions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft
+{
+    class PluginFunctions
+    {
+        public void GlennSays(string what)
+        {
+            Logger.Log(LogType.ConsoleOutput, "Glenn says " + what);
+        }
+    }
+}
---
+++ fCraft/Plugin/PluginManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LuaInterface;
+using System.IO;
+
+namespace fCraft
+{
+    class PluginManager
+    {
+        private static PluginManager instance;
+        private PluginFunctions functions;
+        private Lua lua;
+
+        private PluginManager()
+        {
+            // Empty bitch
+        }
+
+        public static PluginManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PluginManager();
+                instance.Initialize();
+            }
+
+            return instance;
+        }
+
+        private void Initialize()
+        {
+            // Lua, you jelly?
+            lua = new Lua();
+
+            // Initialize functions
+            functions = new PluginFunctions();
+
+            // Register motherfucking functions
+            lua.RegisterFunction("GlennSays", functions, functions.GetType().GetMethod("GlennSays"));
+
+            if (!Directory.Exists("plugins"))
+            {
+                Directory.CreateDirectory("plugins");
+            }
+
+            // Load plugins
+            foreach (String file in Directory.GetFiles("plugins"))
+            {
+                Logger.Log(LogType.ConsoleOutput, "Loading plugin: " + file);
+                lua.DoFile(file);
+            }
+        }
+    }
+}
