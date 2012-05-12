034c0b882d2678730de0e04a4ec0c68ff7cd90b7
Jon Baker <jonty800@gmail.com>
2012-05-12 06:37:20 +0100

    Started on the /DrawScheme command

--- fCraft/Commands/BuildingCommands.cs
+++ fCraft/Commands/BuildingCommands.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using fCraft.Drawing;
 using fCraft.MapConversion;
 using JetBrains.Annotations;
-
+using LibNbt;
+using LibNbt.Exceptions;
+using LibNbt.Queries;
+using LibNbt.Tags;
+using System.IO;
 namespace fCraft {
     /// <summary> Commands for placing specific blocks (solid, water, grass),
     /// and switching block placement modes (paint, bind). </summary>
@@ -94,6 +98,60 @@ namespace fCraft {
             CommandManager.RegisterCommand(CdPlace);
             CommandManager.RegisterCommand(CdTower);
             CommandManager.RegisterCommand(CdCylinder);
+            CommandManager.RegisterCommand(CdDrawScheme);
+        }
+
+        static readonly CommandDescriptor CdDrawScheme = new CommandDescriptor
+        {
+            Name = "DrawScheme",
+            Aliases = new[] { "drs" },
+            Category = CommandCategory.Building,
+            Permissions = new[] { Permission.PlaceAdmincrete },
+            Help = "Toggles the admincrete placement mode. When enabled, any stone block you place is replaced with admincrete.",
+            Handler = test
+        };
+        public static void test(Player player, Command cmd)
+        {
+            if(!File.Exists("C:/users/jb509/desktop/1.schematic"))
+            {
+                player.Message("Nop"); return;
+            }
+            NbtFile file = new NbtFile("C:/users/jb509/desktop/1.schematic");
+            file.RootTag = new NbtCompound("Schematic");
+            file.LoadFile();
+            bool notClassic = false;
+            short width = file.RootTag.Query<NbtShort>("/Schematic/Width").Value;
+            short height = file.RootTag.Query<NbtShort>("/Schematic/Height").Value;
+            short length = file.RootTag.Query<NbtShort>("/Schematic/Length").Value;
+            Byte[] blocks = file.RootTag.Query<NbtByteArray>("/Schematic/Blocks").Value;
+
+            Vector3I pos = player.Position.ToBlockCoords();
+            int i = 0;
+            player.Message("&SDrawing Schematic ({0}x{1}x{2})", length, width, height);
+            for (int x = pos.X; x < width + pos.X; x++)
+            {
+                for (int y = pos.Y; y < length + pos.Y; y++)
+                {
+                    for (int z = pos.Z; z < height + pos.Z; z++)
+                    {
+                        if (Enum.Parse(typeof(Block), ((Block)blocks[i]).ToString(), true) != null)
+                        {
+                            if (!notClassic && blocks[i] > 49)
+                            {
+                                notClassic = true;
+                                player.Message("&WSchematic used is not designed for Minecraft Classic;" +
+                                                " Converting all unsupported blocks with air");
+                            }
+                            if (blocks[i] < 50)
+                            {
+                                player.WorldMap.QueueUpdate(new BlockUpdate(null, (short)x, (short)y, (short)z, (Block)blocks[i]));
+                            }
+                        }
+                        i++;
+                    }
+                }
+            }
+            file.Dispose();
         }
         #region 800Craft
 
