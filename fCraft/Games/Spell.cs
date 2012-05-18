6ada899f46f37c8ed62b0b6e0775573d77234853
Jon Baker <jonty800@gmail.com>
2012-05-18 03:04:39 +0100

    Added an enum for spells

---
+++ fCraft/Games/Spell.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft
+{
+    public enum Spell : byte
+    {
+        Polymorph = 1,
+
+        Impersonation = 2,
+
+        ExplodingTree = 3,
+
+        FrostNova = 4,
+
+        Flash = 5,
+
+        Cleanse = 6,
+
+        Shield = 7,
+
+        Inferno = 8,
+
+        SelfDestruct = 9,
+
+        Silence = 10,
+
+        Freeze = 11,
+    }
+}
