14dedbcba103cbc0c0aaf178d42897f3e1a8a95a
Jonty800 <Jonty800@gmail.com>
2012-05-25 12:39:57 +0100

    Started on the zombie game

---
+++ fCraft/Games/ZombieGame.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fCraft
+{
+    public class ZombieGame
+    {
+        private string _zomb = "&c_Infec&8ted_";
+        private World _world;
+        public ZombieGame(World world)
+        {
+            _world = world;
+        }
+
+        public bool toZombie(Player infector, Player target)
+        {
+            if (target.Immortal) return false;
+            if (infector.iName == _zomb && target.iName != _zomb)
+            {
+                target.iName = _zomb;
+                target.entityChanged = true;
+                _world.Players.Message("{0}&S was &cInfected&S by {1}",
+                    infector.ClassyName, target.ClassyName);
+                return true;
+            }
+            return false;
+        }
+
+        public void RevertNames()
+        {
+            foreach (Player p in _world.Players)
+            {
+                if (p.iName == _zomb){
+                    p.iName = null;
+                    p.entityChanged = true;
+                    p.Message("You are being disinfected");
+                }
+            }
+        }
+
+        public void RevertPlayerName(Player p)
+        {
+            if (p.iName == _zomb)
+            {
+                p.iName = null;
+                p.entityChanged = true;
+                p.Message("You are being disinfected");
+            }
+        }
+    }
+}
