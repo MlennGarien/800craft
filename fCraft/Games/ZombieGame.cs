3c08f75dbacb1711073676565911ed28051a6964
Jonty800 <Jonty800@gmail.com>
2012-05-25 12:57:49 +0100

    More zombie game stuffs

--- fCraft/Games/ZombieGame.cs
+++ fCraft/Games/ZombieGame.cs
@@ -7,22 +7,53 @@ namespace fCraft
 {
     public class ZombieGame
     {
-        private string _zomb = "&c_Infec&8ted_";
+        private const string _zomb = "&c_Infec&8ted_";
         private World _world;
+        private int _humanCount;
         public ZombieGame(World world)
         {
             _world = world;
+            _humanCount = _world.Players.Count();
+        }
+
+        public void Start()
+        {
+            _world.gameMode = World.GameMode.ZombieSurvival; //set the game mode
+            _humanCount = _world.Players.Where(p => p.iName != _zomb).Count(); //count all players
+            _world.Players.Message("&WThe game has begun! Run!!!");
+        }
+
+        public void RandomPick()
+        {
+            Random rand = new Random();
+            int pick = rand.Next(0, _world.Players.Count() + 1);
+            Player picked = _world.Players[pick];
+            toZombie(null, picked);
+        }
+
+        public void Interval()
+        {
+            if (_world.gameMode != World.GameMode.ZombieSurvival){
+                return;
+            }
+            _humanCount = _world.Players.Where(p => p.iName != _zomb).Count(); //refresh incase new players joined
+            _world.Players.Message("&WThere are {0} humans left", _humanCount.ToString());
         }
 
         public bool toZombie(Player infector, Player target)
         {
-            if (target.Immortal) return false;
             if (infector.iName == _zomb && target.iName != _zomb)
             {
                 target.iName = _zomb;
                 target.entityChanged = true;
-                _world.Players.Message("{0}&S was &cInfected&S by {1}",
-                    infector.ClassyName, target.ClassyName);
+                if (infector != null)
+                {
+                    _world.Players.Message("{0}&S was &cInfected&S by {1}",
+                        target.ClassyName, infector.ClassyName);
+                }else{
+                    _world.Players.Message("{0}&S has been the first to get &cInfected. &CPanick!!!!!",
+                        infector.ClassyName);
+                }
                 return true;
             }
             return false;
