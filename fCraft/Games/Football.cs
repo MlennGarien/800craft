6c7fb1681bab3a4614e9bd41e4d587d0f1d7f8c2
Jonty800 <Jonty800@gmail.com>
2012-06-05 10:08:25 +0100

    test commit

--- fCraft/Games/Football.cs
+++ fCraft/Games/Football.cs
@@ -1,4 +1,5 @@
-﻿//Copyright (C) <2012>  <Jon Baker, Glenn Mariën and Lao Tszy>
+﻿//temp
+//Copyright (C) <2012>  <Jon Baker, Glenn Mariën and Lao Tszy>
 
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
