e53c5b01115c94c319243f89ef7b7ae6cbf6eee6
Jonty800 <Jonty800@gmail.com>
2012-07-17 11:25:27 +0100

    Added /Nick as an easier way to change a player's name

--- fCraft/Commands/MaintenanceCommands.cs
+++ fCraft/Commands/MaintenanceCommands.cs
@@ -32,6 +32,8 @@ namespace fCraft {
 
             CommandManager.RegisterCommand(CdFixRealms);
 
+            CommandManager.RegisterCommand(CdNick);
+
 #if DEBUG
             CommandManager.RegisterCommand( new CommandDescriptor {
                 Name = "BUM",
@@ -114,6 +116,71 @@ namespace fCraft {
                 }
             }
             player.Message("Converted {0} worlds to Realms", Count.ToString());
+        }
+
+        static readonly CommandDescriptor CdNick = new CommandDescriptor
+        {
+            Name = "Nick",
+            Category = CommandCategory.Maintenance,
+            IsConsoleSafe = true,
+            Permissions = new[] { Permission.EditPlayerDB },
+            Usage = "/Nick PlayerName DisplayedName",
+            Help = "A shortcut for /Setinfo DisplayedName, it changes a player's displayedName",
+            Handler = NickHandler
+        };
+
+        static void NickHandler(Player player, Command cmd)
+        {
+            string targetName = cmd.Next();
+            string propertyName = cmd.Next();
+            string valName = cmd.NextAll();
+
+            if (targetName == null || propertyName == null)
+            {
+                CdNick.PrintUsage(player);
+                return;
+            }
+
+            PlayerInfo info = PlayerDB.FindPlayerInfoOrPrintMatches(player, targetName);
+            if (info == null) return;
+            string oldDisplayedName = info.DisplayedName;
+            if (valName.Length == 0) valName = null;
+            if (valName == info.DisplayedName)
+            {
+                if (valName == null)
+                {
+                    player.Message("Nick: DisplayedName for {0} is not set.",
+                                    info.Name);
+                }
+                else
+                {
+                    player.Message("Nick: DisplayedName for {0} is already set to \"{1}&S\"",
+                                    info.Name,
+                                    valName);
+                }
+                return;
+            }
+            info.DisplayedName = valName;
+
+            if (oldDisplayedName == null)
+            {
+                player.Message("Nick: DisplayedName for {0} set to \"{1}&S\"",
+                                info.Name,
+                                valName);
+            }
+            else if (valName == null)
+            {
+                player.Message("Nick: DisplayedName for {0} was reset (was \"{1}&S\")",
+                                info.Name,
+                                oldDisplayedName);
+            }
+            else
+            {
+                player.Message("Nick: DisplayedName for {0} changed from \"{1}&S\" to \"{2}&S\"",
+                                info.Name,
+                                oldDisplayedName,
+                                valName);
+            }
         }
         #endregion
 
