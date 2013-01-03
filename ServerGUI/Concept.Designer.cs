70f35549b25946540de4b864fc89db4a103dae36
Jonty800 <Jonty800@gmail.com>
2013-01-03 01:12:12 +0000

    Added missing files

---
+++ ServerGUI/Concept.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace fCraft.ServerGUI {
+    partial class Concept {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing ) {
+            if ( disposing && ( components != null ) ) {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Concept));
+            this.MinimizeButton = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.PictureBox();
+            this.ConsoleButton = new System.Windows.Forms.PictureBox();
+            this.PlayersButton = new System.Windows.Forms.PictureBox();
+            this.ConfigButton = new System.Windows.Forms.PictureBox();
+            this.WorldsButton = new System.Windows.Forms.PictureBox();
+            this.AboutButton = new System.Windows.Forms.PictureBox();
+            this.PlayButton = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MainControl = new TablessControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsoleButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayersButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConfigButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorldsButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AboutButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayButton)).BeginInit();
+            this.MainControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.Color.Transparent;
+            this.MinimizeButton.Image = ((System.Drawing.Image)(resources.GetObject("MinimizeButton.Image")));
+            this.MinimizeButton.Location = new System.Drawing.Point(737, 2);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(25, 25);
+            this.MinimizeButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MinimizeButton.TabIndex = 0;
+            this.MinimizeButton.TabStop = false;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.Color.Transparent;
+            this.CloseButton.Image = ((System.Drawing.Image)(resources.GetObject("CloseButton.Image")));
+            this.CloseButton.Location = new System.Drawing.Point(768, 2);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(25, 25);
+            this.CloseButton.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.TabStop = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // ConsoleButton
+            // 
+            this.ConsoleButton.Location = new System.Drawing.Point(561, 80);
+            this.ConsoleButton.Name = "ConsoleButton";
+            this.ConsoleButton.Size = new System.Drawing.Size(110, 60);
+            this.ConsoleButton.TabIndex = 2;
+            this.ConsoleButton.TabStop = false;
+            this.ConsoleButton.Click += new System.EventHandler(this.ConsoleButton_Click);
+            // 
+            // PlayersButton
+            // 
+            this.PlayersButton.Location = new System.Drawing.Point(680, 80);
+            this.PlayersButton.Name = "PlayersButton";
+            this.PlayersButton.Size = new System.Drawing.Size(110, 60);
+            this.PlayersButton.TabIndex = 3;
+            this.PlayersButton.TabStop = false;
+            this.PlayersButton.Click += new System.EventHandler(this.PlayersButton_Click);
+            // 
+            // ConfigButton
+            // 
+            this.ConfigButton.Location = new System.Drawing.Point(680, 157);
+            this.ConfigButton.Name = "ConfigButton";
+            this.ConfigButton.Size = new System.Drawing.Size(110, 60);
+            this.ConfigButton.TabIndex = 5;
+            this.ConfigButton.TabStop = false;
+            this.ConfigButton.Click += new System.EventHandler(this.ConfigButton_Click);
+            // 
+            // WorldsButton
+            // 
+            this.WorldsButton.Location = new System.Drawing.Point(561, 157);
+            this.WorldsButton.Name = "WorldsButton";
+            this.WorldsButton.Size = new System.Drawing.Size(110, 60);
+            this.WorldsButton.TabIndex = 4;
+            this.WorldsButton.TabStop = false;
+            this.WorldsButton.Click += new System.EventHandler(this.WorldsButton_Click);
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.Location = new System.Drawing.Point(680, 232);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(110, 60);
+            this.AboutButton.TabIndex = 7;
+            this.AboutButton.TabStop = false;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(561, 232);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(110, 60);
+            this.PlayButton.TabIndex = 6;
+            this.PlayButton.TabStop = false;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "800";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(61, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 24);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Craft";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panel1.ForeColor = System.Drawing.Color.Transparent;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 31);
+            this.panel1.TabIndex = 11;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_Click);
+            // 
+            // MainControl
+            // 
+            this.MainControl.Controls.Add(this.tabPage1);
+            this.MainControl.Controls.Add(this.tabPage2);
+            this.MainControl.Controls.Add(this.tabPage3);
+            this.MainControl.Controls.Add(this.tabPage4);
+            this.MainControl.Controls.Add(this.tabPage5);
+            this.MainControl.Location = new System.Drawing.Point(12, 58);
+            this.MainControl.Name = "MainControl";
+            this.MainControl.SelectedIndex = 0;
+            this.MainControl.Size = new System.Drawing.Size(531, 398);
+            this.MainControl.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(523, 372);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(273, 93);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.listBox2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(523, 372);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(21, 23);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(523, 372);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(523, 372);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(523, 372);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "tabPage5";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // Concept
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MainControl);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.ConfigButton);
+            this.Controls.Add(this.WorldsButton);
+            this.Controls.Add(this.PlayersButton);
+            this.Controls.Add(this.ConsoleButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.MinimizeButton);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Concept";
+            this.Text = "Concept";
+            ((System.ComponentModel.ISupportInitialize)(this.MinimizeButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CloseButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConsoleButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayersButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ConfigButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WorldsButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AboutButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayButton)).EndInit();
+            this.MainControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox MinimizeButton;
+        private System.Windows.Forms.PictureBox CloseButton;
+        private System.Windows.Forms.PictureBox ConsoleButton;
+        private System.Windows.Forms.PictureBox PlayersButton;
+        private System.Windows.Forms.PictureBox ConfigButton;
+        private System.Windows.Forms.PictureBox WorldsButton;
+        private System.Windows.Forms.PictureBox AboutButton;
+        private System.Windows.Forms.PictureBox PlayButton;
+        private TablessControl MainControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ ServerGUI/Concept.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using fCraft.GUI;
+using System.Drawing.Text;
+using fCraft.ServerGUI.Properties;
+using System.Runtime.InteropServices;
+
+namespace fCraft.ServerGUI {
+    public partial class Concept : Form {
+        /// <summary>
+        /// Creates a new Form of type "concept"
+        /// MainControl is modified and uses TablessControl, 
+        /// meaning the tab titles do not show
+        /// </summary>
+        public Concept () {
+            InitializeComponent();
+            SetFonts();
+        }
+
+        static PrivateFontCollection Fonts;
+        static Font MinecraftFont;
+        unsafe void SetFonts () {
+            Fonts = new PrivateFontCollection();
+            fixed ( byte* fontPointer = Resources.minecraft ) {
+                Fonts.AddMemoryFont( ( IntPtr )fontPointer, Resources.minecraft.Length );
+                
+            }
+            MinecraftFont = new Font( Fonts.Families[0], 14, FontStyle.Bold );
+            label1.Font = MinecraftFont;
+                label2.Font = MinecraftFont;
+        }
+
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute( "user32.dll" )]
+        public static extern int SendMessage ( IntPtr hWnd,
+                         int Msg, int wParam, int lParam );
+        [DllImportAttribute( "user32.dll" )]
+        public static extern bool ReleaseCapture ();
+
+        private void CloseButton_Click ( object sender, EventArgs e ) {
+            Environment.Exit( 1 );//check waht exit codes are lal
+        }
+
+        private void MinimizeButton_Click ( object sender, EventArgs e ) {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ConsoleButton_Click ( object sender, EventArgs e ) {
+            MainControl.SelectedIndex = 0;
+        }
+
+        private void PlayersButton_Click ( object sender, EventArgs e ) {
+            MainControl.SelectedIndex = 1;
+        }
+
+        private void WorldsButton_Click ( object sender, EventArgs e ) {
+            MainControl.SelectedIndex = 2;
+        }
+
+        private void ConfigButton_Click ( object sender, EventArgs e ) {
+            MainControl.SelectedIndex = 3;
+        }
+
+        /// <summary>
+        /// This needs two images - one with "unavailable" and another with "online" 
+        /// Clickable when "online"
+        /// Opens the server in the browser
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void PlayButton_Click ( object sender, EventArgs e ) {
+            MainControl.SelectedIndex = 4;
+        }
+
+        private void AboutButton_Click ( object sender, EventArgs e ) {
+            //run aboutwindow
+            new AboutWindow().Show();
+        }
+
+        private void panel1_Click ( object sender, MouseEventArgs e ) {
+            if ( e.Button == MouseButtons.Left ) {
+                ReleaseCapture();
+                SendMessage( Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0 );
+            }
+        }
+    }
+}
