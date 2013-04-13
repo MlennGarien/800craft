75404cfb74506b4f289c018ad2a948e022be3e5b
Jonty800 <Jonty800@gmail.com>
2013-04-13 14:56:09 +0100

    Catch new heartbeat exceptions and do nothing

--- fCraft/Network/Heartbeat.cs
+++ fCraft/Network/Heartbeat.cs
@@ -99,38 +99,41 @@ namespace fCraft
             if ( Server.Uri == null ) return;
             string uri = "http://800craft.webuda.com/Heartbeat.php";
             // create a request
-            HttpWebRequest request = (HttpWebRequest)
-            WebRequest.Create(uri);
-            request.Method = "POST";
+            try {
+                HttpWebRequest request = ( HttpWebRequest )WebRequest.Create( uri );
+                request.Method = "POST";
 
-            // turn request string into a byte stream
-            byte[] postBytes = Encoding.ASCII.GetBytes(string.Format("ServerName={0}&Url={1}&Players={2}&MaxPlayers={3}&Uptime={4}",
-                             Uri.EscapeDataString(ConfigKey.ServerName.GetString()),
-                             Server.Uri,
-                             Server.Players.Length,
-                             ConfigKey.MaxPlayers.GetInt(),
-                             DateTime.UtcNow.Subtract(Server.StartTime).TotalMinutes));
+                // turn request string into a byte stream
+                byte[] postBytes = Encoding.ASCII.GetBytes( string.Format( "ServerName={0}&Url={1}&Players={2}&MaxPlayers={3}&Uptime={4}",
+                                 Uri.EscapeDataString( ConfigKey.ServerName.GetString() ),
+                                 Server.Uri,
+                                 Server.Players.Length,
+                                 ConfigKey.MaxPlayers.GetInt(),
+                                 DateTime.UtcNow.Subtract( Server.StartTime ).TotalMinutes ) );
 
-            request.ContentType = "application/x-www-form-urlencoded";
-            request.CachePolicy = new System.Net.Cache.RequestCachePolicy(System.Net.Cache.RequestCacheLevel.NoCacheNoStore);
-            request.ContentLength = postBytes.Length;
-            request.Timeout = 5000;
-            Stream requestStream = request.GetRequestStream();
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.CachePolicy = new System.Net.Cache.RequestCachePolicy( System.Net.Cache.RequestCacheLevel.NoCacheNoStore );
+                request.ContentLength = postBytes.Length;
+                request.Timeout = 5000;
+                Stream requestStream = request.GetRequestStream();
 
-            // send it
-            requestStream.Write(postBytes, 0, postBytes.Length);
-            requestStream.Flush();
-            requestStream.Close();
-           /* try
-            {
-               HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-               Logger.LogToConsole(new StreamReader(response.GetResponseStream()).ReadToEnd());
-               Logger.LogToConsole(response.StatusCode + "\n");
+                // send it
+                requestStream.Write( postBytes, 0, postBytes.Length );
+                requestStream.Flush();
+                requestStream.Close();
+                /* try
+                 {
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    Logger.LogToConsole(new StreamReader(response.GetResponseStream()).ReadToEnd());
+                    Logger.LogToConsole(response.StatusCode + "\n");
+                 }
+                 catch (Exception ex)
+                 {
+                     Logger.LogToConsole("" + ex);
+                 }*/
+            } catch ( Exception e ) {
+                //do nothing, server is probably down and host doesnt care
             }
-            catch (Exception ex)
-            {
-                Logger.LogToConsole("" + ex);
-            }*/
         }
 
 
